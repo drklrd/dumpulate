9b948736de5fcddf8b97d6d7a0b1c164b000cfeb
drklrd <srvbhattarai@gmail.com>
2016-12-01 22:21:52 +0545

    add support for radio types

--- contentscript.js
+++ contentscript.js
@@ -2,6 +2,8 @@ var inputFields = document.getElementsByTagName("input");
 var selectFields = document.getElementsByTagName("select");
 var textAreas = document.getElementsByTagName("textarea");
 
+var radios={};
+
 if (inputFields && inputFields.length) {
 	for (field = 0; field < inputFields.length; field++) {
 		var type = inputFields[field].type;
@@ -39,11 +41,24 @@ if (inputFields && inputFields.length) {
 
 				case "text":
 
-					inputFields[field].value = "test";
+					inputFields[field].value = "www.google.com";
 					break;
 
 				case "url":
+					
 					inputFields[field].value = "www.google.com";
+					break;
+
+				case "radio":
+
+					if(inputFields[field].name){
+						if(!radios[inputFields[field].name]){
+							radios[inputFields[field].name] = [inputFields[field]];
+						}else{
+							radios[inputFields[field].name].push(inputFields[field]);
+						}
+					}
+
 					break;
 
 				default:
@@ -54,18 +69,41 @@ if (inputFields && inputFields.length) {
 		}
 
 	}
+}else{
+	inputFields = [];
 }
 
+// if(Object.keys(radios).length){
+// 	for(radio in radios){
+// 		if(radios[radio].length){
+// 			radios[radio][Math.floor(Math.random()*radios[radio].length)].checked=true;
+// 		}
+// 	}
+// }
 
 
 if (selectFields && selectFields.length) {
 	for (field = 0; field < selectFields.length; field++) {
 		selectFields[field].value = selectFields[field].options[Math.floor(Math.random() * selectFields[field].length)].text;
 	}
+}else{
+	selectFields = [];
 }
 
 if (textAreas && textAreas.length) {
 	for (area = 0; area < textAreas.length; area++) {
 		textAreas[area].value = "Who lives in a pineapple under the sea ??? Spongebob squarepants !"
 	}
+}else{
+	textAreas = [];
 }
+
+
+// var validators = inputFields.concat(selectFields,textAreas)
+
+// for (field = 0; field < validators.length; field++) {
+// 	if(!validators[field].checkValidity()){
+// 		validators[field].style.borderColor="red"
+// 	}
+	
+// }
